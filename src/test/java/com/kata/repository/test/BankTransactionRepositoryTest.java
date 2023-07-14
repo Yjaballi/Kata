7c2415158b58d49980b357dfe9746ef18841ab9c
Yjaballi <jaballiyoussef4@gmail.com>
2023-07-14 16:19:57 +0100

    reorganize package to fix tests issues

--- src/test/java/com/kata/repository/test/BankTransactionRepositoryTest.java
+++ src/test/java/com/kata/repository/test/BankTransactionRepositoryTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
 
 @DataJpaTest
-@ActiveProfiles("test")
 public class BankTransactionRepositoryTest {
 
     @Autowired
